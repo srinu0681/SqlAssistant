df10d9e4bcbe86b90a5d585981b717ba356ec020
Srinivas Sripathi <srinis8186@outlook.com>
2024-01-11 20:50:12 -0600

    Cleanedup code

--- SqlAssistant/Bot/SemanticKernelBot.cs
+++ SqlAssistant/Bot/SemanticKernelBot.cs
@@ -1,20 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using Azure.AI.OpenAI;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Planning;
-using Newtonsoft.Json;
-using SqlAssistant;
 using SqlAssistant.Plugins;
-using Azure.Monitor.OpenTelemetry.Exporter;
-using Microsoft.Extensions.Options;
 
 namespace SqlAssistant.Bot;
 
@@ -23,11 +15,7 @@ public class SemanticKernelBot : StateManagementBot
     private Kernel _kernel;
     private FunctionCallingStepwisePlanner _planner;
     private ConfigOptions _configOptions;
-    private readonly AzureOpenAITextEmbeddingGenerationService _embeddingsClient;
     private readonly SqlConnectionFactory _sqlConnectionFactory;
-    private readonly string _welcomeMessage;
-    private readonly List<string> _suggestedQuestions;
-    private readonly string _systemMessage;
     private readonly ILogger<SemanticKernelBot> _logger;
     private readonly GraphApiEmailClient _graphApiEmailClient;
     private readonly ExcelBuilder _excelBuilder;
@@ -47,12 +35,6 @@ public class SemanticKernelBot : StateManagementBot
         _configOptions = configOptions;
         _logger = logger;
         _kernel = kernel;
-        //_welcomeMessage = _configOptions.PROMPT_WELCOME_MESSAGE;
-        //_suggestedQuestions = System.Text.Json.JsonSerializer.Deserialize<List<string>>(_configOptions.PROMPT_SUGGESTED_QUESTIONS);
-        _systemMessage = new StreamReader("./Prompts/StepWiseStep/GeneratePlan.yaml")
-                            .ReadToEnd();
-        //    .Replace("{{PROMPT_SYSTEM_MESSAGE}}", config.GetValue<string>("PROMPT_SYSTEM_MESSAGE"));
-
         _sqlConnectionFactory = sqlConnectionFactory;
         _graphApiEmailClient = graphApiEmailClient;
         _excelBuilder = excelBuilder;
@@ -75,10 +57,10 @@ public class SemanticKernelBot : StateManagementBot
         await turnContext.SendActivityAsync(new Activity()
         {
             Type = "message",
-            Text = _welcomeMessage,
+            Text = "Welcome to SQLAssistant Bot! Ask me anything to get started.",
             SuggestedActions = new SuggestedActions()
             {
-                Actions = _suggestedQuestions
+                Actions = new List<string> { "How many customers are in the AdventureWorksLT database", "What are the top 5 highly sold products" }
                     .Select(value => new CardAction(type: "postBack", value: value))
                     .ToList()
             }
@@ -96,7 +78,6 @@ public class SemanticKernelBot : StateManagementBot
 
         var stepwiseConfig = new FunctionCallingStepwisePlannerConfig
         {
-            //GetPromptTemplate = () => _systemMessage,
             MaxIterations = 5,
             MaxTokens = 8000,
         };
@@ -110,41 +91,6 @@ public class SemanticKernelBot : StateManagementBot
 
         _logger.LogInformation(new EventId(1001, "EndPlanExecution"), "{Final Answer} | {Chat history}", result.FinalAnswer, System.Text.Json.JsonSerializer.Serialize(result.ChatHistory));
 
-        //var replyText = $"FinalAnswer:\n{result.FinalAnswer}\nChat history:\n{System.Text.Json.JsonSerializer.Serialize(result.ChatHistory)}";
-        //return replyText;
         return result.FinalAnswer;
-
-        //var res = await kernel.RunAsync(plan);
-        //var stepsTaken = JsonConvert.DeserializeObject<Step[]>(res.FunctionResults.First().Metadata["stepsTaken"].ToString());
-        //return stepsTaken[stepsTaken.Length - 1].final_answer;
     }
 }
-
-//private Kernel GetKernel(ConversationData conversationData, ITurnContext<IMessageActivity> turnContext)
-//{
-//    _loggerFactory = LoggerFactory.Create(builder =>
-//    {
-//        // Add OpenTelemetry as a logging provider
-//        builder.AddOpenTelemetry(options =>
-//        {
-//            options.AddAzureMonitorLogExporter(options => options.ConnectionString = _config.GetValue<string>("ApplicationInsights_ConnectionString"));
-//            // Format log messages. This is default to false.
-//            options.IncludeFormattedMessage = true;
-//        });
-//        builder.SetMinimumLevel(LogLevel.Information);
-//    });
-
-//    var builder = Kernel.CreateBuilder();
-//    builder.Services.AddSingleton(_loggerFactory);
-
-//    _kernel = builder.AddAzureOpenAIChatCompletion(_config.GetValue<string>("OpenAI_GPTModelName"), _config.GetValue<string>("OpenAI_EndPoint"), _config.GetValue<string>("OpenAI_Key"))
-//                     .Build();
-
-//    if (_sqlConnectionFactory != null)
-//    {
-//        _kernel.ImportPluginFromObject(new SQLPlugin(conversationData, turnContext, _sqlConnectionFactory), "SQLPlugin");
-//    }
-
-//    _kernel.ImportPluginFromObject(new ExcelExportPlugin(conversationData, turnContext), "ExcelExportPlugin");
-//    return _kernel;
-//}
--- SqlAssistant/Bot/StateManagementBot.cs
+++ SqlAssistant/Bot/StateManagementBot.cs
@@ -1,23 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Generic;
-using System.Net;
 using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Teams;
-using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
 using Microsoft.Bot.Schema.Teams;
-using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json.Linq;
-using SqlAssistant;
 using System.Net.Http.Headers;
-using Microsoft.Extensions.Options;
 
 namespace SqlAssistant.Bot;
 
@@ -50,7 +40,7 @@ public class StateManagementBot : TeamsActivityHandler
 
     protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
     {
-        await turnContext.SendActivityAsync("Welcome to State Bot Sample. Type anything to get started.");
+        await turnContext.SendActivityAsync("Welcome to SQLAssistant Bot. Type anything to get started.");
     }
 
     protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
@@ -65,7 +55,6 @@ public class StateManagementBot : TeamsActivityHandler
         // Clear conversation
         if (turnContext.Activity.Text != null && turnContext.Activity.Text.ToLower() == "clear") {
             conversationData.History.Clear();
-            //conversationData.Attachments.Clear();
             await turnContext.SendActivityAsync("Conversation context cleared");
             return;
         }
@@ -156,7 +145,7 @@ public class StateManagementBot : TeamsActivityHandler
             ContentUrl = fileConsentCardResponse.UploadInfo.ContentUrl,
         };
 
-        var reply = MessageFactory.Text($"<b>Upload the query results excel sheet to your OneDrive successfully.</b>");
+        var reply = MessageFactory.Text($"<b>Uploaded the query results excel sheet to your OneDrive successfully.</b>");
         reply.TextFormat = "xml";
         reply.Attachments = new List<Attachment> { asAttachment };
 
